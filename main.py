7b91451899f44055631c301867024d0b4cd3c3be
harshitkh3 <harshitkhandelwal2006@gmail.com>
2026-03-09 17:40:10 +0530

    working-fine

--- main.py
+++ main.py
@@ -11,21 +11,47 @@ def pass_gen (dig,hasdigits=True,hasspecial=True):
         char+=digits
     if(hasspecial):
         char+=special
-    pwd=""
+    pwd=[]
     c_dig=False
     c_sp=False
     criteria=False
 
-    while not criteria or len(pwd)<dig:
-        nexc=random.choice(char)
-        pwd+=nexc
-        if nexc in digits:
-            c_dig=True
-        if nexc in special:
-            c_dig=True
+    if hasdigits==True:
+        pwd.append(random.choice(digits))
+    if hasspecial==True:
+        pwd.append(random.choice(special))
+    while len(pwd)<dig:
+        pwd.append(random.choice(char))
+    
+    random.shuffle(pwd)
+
+    return "".join(pwd)
+    
+# def handle_ip(dig_ip,spc_ip):
+#     if (dig_ip=='y' or dig_ip=='Y'):
+#      dig_ip=True
+#     else:
+#      dig_ip==False
+#     if (spc_ip=='y' or spc_ip=='Y'):
+#         spc_ip=True
+#     else:
+#         spc_ip==False
+
         
 
 
-
-pass_gen(5,True,True)
+num=int(input('Enter the minimum length of the password : '))
+dig_ip=input("Do you want digits in your password (y/n) : ")
+spc_ip=input("Do you want special characters in your password (y/n) : ")
+if (dig_ip=='y' or dig_ip=='Y'):
+    dig_ip=True
+else:
+    dig_ip=False
+if (spc_ip=='y' or spc_ip=='Y'):
+    spc_ip=True
+else:
+    spc_ip=False
+ans=pass_gen(num,dig_ip,spc_ip)
+print('The generated password is :')
+print(ans)
 
